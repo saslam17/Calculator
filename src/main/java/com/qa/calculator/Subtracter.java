839814da84b2367e54ced715974897fb50ec3d75
saslam17 <suleman_awan@hotmail.co.uk>
2018-04-19 10:54:55 +0100

    Added subtract method through TDD

--- src/main/java/com/qa/calculator/Subtracter.java
+++ src/main/java/com/qa/calculator/Subtracter.java
@@ -2,9 +2,9 @@ package com.qa.calculator;
 
 public class Subtracter {
 
-	public int subtract(int numb1, int numb2) {
+	public static int subtract(int numb1, int numb2) {
 		
-		return numb1 + numb2;
+		return numb1 - numb2;
 	}
 
 }
